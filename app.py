52a2e60e9dbad5c7316eaf82fe86ba9da7527070
akshaysonawaneS <akshaysonawane10526@gmail.com>
2020-03-02 19:28:07 +0530

    added flask app

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+from flask import *
+from main import pdfExtractor
+UPLOAD_FOLDER = '/home/Akshay/Projects/Python/Summurize/uploads'
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def upload():
+    return render_template("upload_form.html")
+
+@app.route('/success', methods = ['POST'])
+def success():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
+        summ = pdfExtractor(f.filename)
+        return render_template("success.html", name=f.filename, sum=summ)
+
+
+if __name__ ==  '__main__':
+    app.run()
+
--- main.py
+++ main.py
@@ -1,24 +1,75 @@
 import PyPDF2
 from nltk.corpus import stopwords
-from nltk.cluster.util import cosine_distance
-import numpy as np
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize, sent_tokenize
 
-text =[]
+text = []
 
-def main():
-    pdfExtractor()
+def create_frequency_table(text_string) -> dict:
 
-def summ(text1):
-    sentence = list(text1.split("."))
-    sentences = []
-    for i in sentence:
-        sentences.append(i.replace("[^a-zA-Z]", " ").split(" "))
+    stopWords = set(stopwords.words("english"))
+    words = word_tokenize(text_string)
+    ps = PorterStemmer()
 
-    sentences.pop()
-    print(sentences)
+    freqTable = dict()
+    for word in words:
+        word = ps.stem(word)
+        if word in stopWords:
+            continue
+        if word in freqTable:
+            freqTable[word] += 1
+        else:
+            freqTable[word] = 1
 
-def pdfExtractor():
-    pdfFileObj = open('sample.pdf', 'rb')
+    return freqTable
+
+
+def generate_summary(sentences, sentenceValue, threshold):
+    sentence_count = 0
+    summary = ''
+
+    for sentence in sentences:
+        if sentence[:10] in sentenceValue and sentenceValue[sentence[:10]] > (threshold):
+            summary += " " + sentence
+            sentence_count += 1
+
+    return summary
+
+
+
+def find_average_score(sentenceValue):
+    sumValues = 0
+    for entry in sentenceValue:
+        sumValues += sentenceValue[entry]
+
+    # Average value of a sentence from original text
+    average = int(sumValues / len(sentenceValue))
+
+    return average
+
+def score_sentences(sentences, freqTable):
+    sentenceValue = dict()
+
+    for sentence in sentences:
+        word_count_in_sentence = (len(word_tokenize(sentence)))
+        for wordValue in freqTable:
+            if wordValue in sentence.lower():
+                if sentence[:10] in sentenceValue:
+                    sentenceValue[sentence[:10]] += freqTable[wordValue]
+                else:
+                    sentenceValue[sentence[:10]] = freqTable[wordValue]
+
+        sentenceValue[sentence[:10]] = sentenceValue[sentence[:10]] // word_count_in_sentence
+
+    return sentenceValue
+
+# def main():
+    # path = "sample.pdf"
+    # pdfExtractor(path)
+
+def pdfExtractor(path):
+    path = "uploads/" + path
+    pdfFileObj = open(path, 'rb')
     pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 
     pgno = pdfReader.numPages
@@ -27,10 +78,18 @@ def pdfExtractor():
         text.append(pageObj.extractText())
 
     text1 = ''.join(text)
-    summ(text1)
+
+    freq_table = create_frequency_table(text1)                                  # Creating frequency table
+    sentences = sent_tokenize(text1)                                            # Tokenize Sentence
+    sentence_scores = score_sentences(sentences, freq_table)
+    threshold = find_average_score(sentence_scores)
+    summary = generate_summary(sentences, sentence_scores, 1.5 * threshold)
+    print(text1)
+    print("----------------------------After Summurize---------------------------------------")
+    print(summary)
+
     pdfFileObj.close()
+    return summary
 
-
-if __name__ == "__main__":
-    main()
-
+# if __name__ == "__main__":
+#     main()
